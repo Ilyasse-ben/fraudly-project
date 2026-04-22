2e533d820ec3b544202ebfcb7996f2d9140bb68d
ELHAID Yousef <elhaidyousef@gmail.com>
2026-04-22 22:55:48 +0100

    feat: setup auth-service dependencies and config

--- Backend-Spring/authentification-service/src/main/java/net/enset/authentificationservice/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package net.enset.authentificationservice.entity;
-
-public enum Role {
-    ADMIN,
-    STUDENT
-}
--- Backend-Spring/authentification-service/src/main/java/net/enset/authentificationservice/repository/UesrRepoditory.java
+++
@@ -1,7 +0,0 @@
-package net.enset.authentificationservice.repository;
-
-import net.enset.authentificationservice.entity.User;
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-public interface UesrRepoditory extends JpaRepositoryImplementation<User,Long> {
-}
